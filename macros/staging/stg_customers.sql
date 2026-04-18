b957719c49f1d86db3f05448c601f184e72c95d8
CarlosVilladiego <127449739+CaVilladiego@users.noreply.github.com>
2026-04-17 20:44:48 -0500

    Delete macros/staging directory

--- macros/staging/stg_customers.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT
-    customer_id AS id_cliente,
-    name AS nombre,
-    country_code AS pais,
-    city AS ciudad,
-    customer_tier AS clase,
-FROM {{ source('raw','raw_customers_updated') }}
